d03941fdeffc7669d8cea1998f1e03df7e7e5bf2
Russell Osborne <russosborn@gmail.com>
2014-04-20 21:26:13 -0400

    Tweaked Output to include dependent hash.

--- lib/gitwakatime/cli.rb
+++ lib/gitwakatime/cli.rb
@@ -15,12 +15,23 @@ module  GitWakaTime
       @timer    = Timer.new(@mapper.commits)
 
       @commits_with_duration = @timer.process
-      Log.new("Total Presented Time #{ChronicDuration.output @commits_with_duration.map { |c| c.time_in_seconds }.reduce { |a, e| a + e }.to_f} ".red)
+
+      Log.new("Total Time #{ChronicDuration.output @commits_with_duration.map { |c| c.time_in_seconds }.reduce { |a, e| a + e }.to_f} ".red)
       @commits_with_duration.each do |commit|
-        puts ' %-8s %-30s %-80s'.green % [commit.sha[0..8], ChronicDuration.output(commit.time_in_seconds), commit.message]
+        puts ' %-8s %8s %-30s %-80s'.green %
+        [
+          commit.sha[0..8],
+          commit.date,
+          ChronicDuration.output(commit.time_in_seconds),
+          commit.message
+        ]
 
         commit.files.each do |file|
-          puts '    %-40s %-20s'.blue % [ChronicDuration.output(file[:time_in_seconds].to_f), file[:name]]
+          puts '    %-40s %-40s %-20s'.blue % [
+            ChronicDuration.output(file[:time_in_seconds].to_f),
+            file[:name],
+            (file[:dependent_commit].sha[0..8] if file[:dependent_commit])
+          ]
 
         end
       end
