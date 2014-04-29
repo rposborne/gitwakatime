fe60ad72372ae715ca70d5a5a265ea5525873f4b
Russell Osborne <russosborn@gmail.com>
2014-04-29 15:05:37 -0400

    Get Commits for the month (max 100) Adding some Todo's

--- lib/gitwakatime/mapper.rb
+++ lib/gitwakatime/mapper.rb
@@ -1,11 +1,16 @@
 module GitWakaTime
+  # Th
   class Mapper
     attr_accessor :commits, :git
-    def initialize(path, commits = 20)
+    def initialize(path, commits = 100)
       Log.new 'Mapping commits for dependent commits'
       time = Benchmark.realtime do
         @git = Git.open(path)
-        logs =  @git.log(20)
+        # TODO: Expose since timestamp as a CLI option
+        # TODO: Expose number of commits as a CLI option
+        first_of_month = Date.new(Date.today.year, Date.today.month, 1)
+
+        logs =  @git.log(commits).since(first_of_month)
 
         @commits = logs.map do |git_commit|
           Commit.new(@git, git_commit)
