758037dcab400c7cb4dd3ca2b8929f93eabe5ed4
Russell Osborne <russosborn@gmail.com>
2014-04-29 01:18:04 -0400

    Time the mapper.

--- lib/gitwakatime/mapper.rb
+++ lib/gitwakatime/mapper.rb
@@ -1,13 +1,17 @@
 module GitWakaTime
   class Mapper
     attr_accessor :commits, :git
-    def initialize(path)
-      @git = Git.open(path)
-      logs =  @git.log(20)
+    def initialize(path, commits = 20)
+      Log.new 'Mapping commits for dependent commits'
+      time = Benchmark.realtime do
+        @git = Git.open(path)
+        logs =  @git.log(20)
 
-      @commits = logs.map do |git_commit|
-        Commit.new(@git, git_commit)
+        @commits = logs.map do |git_commit|
+          Commit.new(@git, git_commit)
+        end
       end
+      Log.new "Map Completed took #{time}s"
     end
   end
 end
