8dc62dc1c26a49e7078ef1b359983aa7d24251ee
Russell Osborne <russosborn@gmail.com>
2015-02-03 15:20:55 -0500

    Adding some comments.

--- lib/gitwakatime/commited_file.rb
+++ lib/gitwakatime/commited_file.rb
@@ -1,18 +1,18 @@
 module  GitWakaTime
   class CommitedFile < Sequel::Model
     many_to_one :commit
-    # No two committed files should have the sames name + dependent_date
+    # No two committed files should have the same name + dependent_date this
+    # means a split tree, and we should split time between the two, or more, commits.
 
     def before_create
       find_dependent_commit(name)
     end
 
     def to_s
-      format('                 %-20s %-40s %-100s '.blue,
+      format('                        %-20s %-40s %-100s '.blue,
              (dependent_sha[0..8] if dependent_sha),
-             ChronicDuration.output(@time_in_seconds.to_f),
+             ChronicDuration.output(time_in_seconds.to_f),
              name
-
              )
     end
 
@@ -20,7 +20,9 @@ module  GitWakaTime
 
     def find_dependent_commit(name)
       i = 1
-      commit = 1
+
+      # Call git log for path, loop through till we find a valid commit or run
+      # out of commits to check
       commits = load_dependent_commits(name)
       begin
         commit = commits[i]
@@ -29,13 +31,13 @@ module  GitWakaTime
           self.dependent_date = commit.date
           self.dependent_sha = commit.sha
 
+          # This is the magically fix for the split tree issue
           dc = CommitedFile.where(name: name, dependent_sha: commit.sha).first
           dc.update(dependent_date: dc.commit.date) if dc && dc.commit
         end
 
         i += 1
       end until !dependent_sha.nil? || commit.nil?
-      dependent_sha
     end
 
     def allowed_commit(commit)
