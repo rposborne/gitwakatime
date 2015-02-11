1b72d2fbe1bcfc508fc42591e9f6e8468554751f
Russell Osborne <russosborn@gmail.com>
2015-02-10 20:50:56 -0500

    Don't query before wakatime epoch :)

--- lib/gitwakatime/query.rb
+++ lib/gitwakatime/query.rb
@@ -29,17 +29,20 @@ module GitWakaTime
       commits = @commits.map(&:date)
       d_commits = CommitedFile.select(:dependent_date).all.map { |f| f.values[:dependent_date] }.compact
       timestamps = (commits + d_commits.flatten).uniq.sort
-      @start_at = timestamps.first
+
+      # Don't query before the Wakatime Epoch
+      @start_at = timestamps.first >= Time.new(2013, 5, 1) ? timestamps.first : Time.new(2013, 5, 1)
       @end_at = timestamps.last
+
       # Always have a date range great than 1 as the num request will be 0/1 otherwise
-      num_requests = ((timestamps.last.to_date + 1) - timestamps.first.to_date) / @api_limit
+      num_requests = ((@end_at.to_date + 1) - @start_at.to_date) / @api_limit
       i = 0
 
       request_params = num_requests.to_f.ceil.times.map do
 
         params = {
-          start: (timestamps.first.to_date + (i * @api_limit)).to_time.beginning_of_day,
-          end:  (timestamps.first.to_date + ((i + 1) * @api_limit)).to_time.end_of_day,
+          start: (@start_at.to_date + (i * @api_limit)).to_time.beginning_of_day,
+          end:  (@start_at.to_date + ((i + 1) * @api_limit)).to_time.end_of_day,
           project: @project,
           show: 'file,branch,project,time,id'
         }
